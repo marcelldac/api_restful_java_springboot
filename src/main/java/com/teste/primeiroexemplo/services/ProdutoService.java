762a703e3d87db1c4628647f6c214661bb57ba22
marcelldac <dacmarcell@proton.me>
2023-09-22 03:33:25 -0300

    feat: adiciona serviço Produto
    - adiciona metodos do repositório.

---
+++ src/main/java/com/teste/primeiroexemplo/services/ProdutoService.java
@@ -0,0 +1,72 @@
+package com.teste.primeiroexemplo.services;
+
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.teste.primeiroexemplo.model.Produto;
+import com.teste.primeiroexemplo.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+  @Autowired
+  private ProdutoRepository produtoRepository;
+
+  /**
+   * Método para retornar uma lista de produtos.
+   * 
+   * @return lista de produtos.
+   */
+  public List<Produto> obterTodos() {
+    return produtoRepository.obterTodos();
+  }
+
+  /**
+   * Método que retorna o produto encontrado pelo seu id.
+   * 
+   * @param id do produto que será localizado.
+   * @return Retorna um produto caso seja encontrado.
+   */
+  public Optional<Produto> obterPorId(Integer id) {
+    if (id == null) {
+      throw new InputMismatchException("Id não encontrado.");
+    }
+    return produtoRepository.obterPorId(id);
+  }
+
+  /**
+   * Método para adicionar produto na lista.
+   * 
+   * @param produto a ser adicionado.
+   * @return Retorna o produto que foi adicionado na lista.
+   */
+  public Produto adicionar(Produto produto) {
+    return produtoRepository.adicionar(produto);
+  }
+
+  /**
+   * Método para deletar o produto por id.
+   * 
+   * @param id do produto a ser deletado.
+   */
+  public void deletar(Integer id) {
+    produtoRepository.deletar(id);
+  }
+
+  /**
+   * Método para atualizar o produto na lista.
+   * 
+   * @param produto que será atualizado.
+   * @return Retorna o produto após atualizar a lista.
+   */
+  public Produto atualizar(Integer id, Produto produto) {
+
+    produto.setId(id);
+
+    return produtoRepository.atualizar(produto);
+  }
+}
